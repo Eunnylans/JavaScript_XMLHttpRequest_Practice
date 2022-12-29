3444aa8fe0296afcebbe639a8831476744d42186
Eunice Igbinedion <eunice.igbinedion@yahoo.com>
2022-12-29 14:53:28 +0100

    Updating Script File

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const btn = document.querySelector('.bt -country');
 const countriesContainer = document.querySelector('.countries');
 
 const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+request.send();
+
+request.addEventListener('load', function () {});
